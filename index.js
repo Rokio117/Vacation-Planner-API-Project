7e8e583c0e604f0a71f00340a480ab6540ba5959
Nick Hillary <hillan02@gmail.com>
2019-06-23 21:20:44 -0500

    initial turn-in

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+let numSearch = $('#results-length').val()
 const apiKey = 'tLlmdwj7hxSbe01snvsAG6ezl4TbSZqytzqoBFwb'
 const baseUrl = 'https://developer.nps.gov/api/v1/parks'
 const options = {
@@ -10,30 +10,38 @@ const options = {
 function handleSubmit() {
   $('#user-input').submit(function(event){
     event.preventDefault()
+    $('#search-results').empty()
     userState = $('#state-selector').val()
-    createURL(userState);
+    numSearch = $('#results-length').val()
+    createURL(userState, numSearch);
   })
   
 }
 
 
-function createURL(userState) {
-  newUrl =`${baseUrl}?q=${userState}`
+function createURL(userState, num) {
+  newUrl =`${baseUrl}?limit=${num}&q=${userState}&api_key=${apiKey}`
   console.log(newUrl)
   fetchURL(newUrl, options)
 }
 
-function fetchURL(URL, api) {
-  console.log('fetchURL Ran')
-  console.log(options)
-  fetch (URL, api)
+function fetchURL(URL) {
+  fetch (URL)
   .then(response => response.json())
-  .then(responseJson => console.log(responseJson))
+  .then(responseJson => displayResults(responseJson, numSearch))
 }
 
-function displayResults() {
+function displayResults(result, searchNum) {
   console.log('displayResults Ran')
-  //properly displays results to the user
+  for (i = 0; i < searchNum; i++) {
+    $('#search-results').append(
+      `<li class="result">
+      <h2 class="name">${result.data[i].fullName}</h2>
+      <p class="description">${result.data[i].description}</p>
+      <a href "${result.data[i].url}">${result.data[i].url}</a>
+      </li>`
+    )
+  }
 }
 
 $(handleSubmit)
